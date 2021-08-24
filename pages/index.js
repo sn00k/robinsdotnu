af1c57383833b1a6095b54ae9ac9f0daa4a57278
sn00k <robin@robins.nu>
2021-08-24 10:28:15 +0200

    fix flashing icons

--- pages/index.js
+++ pages/index.js
@@ -35,7 +35,7 @@ export default function Home() {
         {!loaded && (
           <div>
             <p>
-              <FontAwesomeIcon icon={faCircleNotch} size="3x" spin />
+              <FontAwesomeIcon icon={faCircleNotch} size="3x" width="48" spin />
             </p>
           </div>
         )}
@@ -50,39 +50,39 @@ export default function Home() {
         {loaded && (
           <div className="arrow bounce">
             <a href="#" onClick={executeScroll}>
-              <FontAwesomeIcon icon={faArrowDown} size="2x" />
+              <FontAwesomeIcon icon={faArrowDown} size="2x" width="28" />
             </a>
           </div>
         )}
       </section>
       <div className="content" ref={contentRef}>
-        <section className="description">
+        <article className="description">
           <h1>Hi, I'm Robin!</h1>
-          <span>Full Stack Software Developer from Sweden.</span>
+          <h3>Full Stack Software Developer from Sweden.</h3>
           <p>Here are some of the tools that I'd like to work more with</p>
           <ul className="brands">
             <li>
-              <FontAwesomeIcon icon={faJsSquare} size="2x" />
+              <FontAwesomeIcon icon={faJsSquare} size="2x" width="28" />
               <span className="sr-only">Javascript</span>
             </li>
             <li>
-              <FontAwesomeIcon icon={faVuejs} size="2x" />
+              <FontAwesomeIcon icon={faVuejs} size="2x" width="28" />
               <span className="sr-only">Vue.js</span>
             </li>
             <li>
-              <FontAwesomeIcon icon={faReact} size="2x" />
+              <FontAwesomeIcon icon={faReact} size="2x" width="32" />
               <span className="sr-only">React.js</span>
             </li>
             <li>
-              <FontAwesomeIcon icon={faDocker} size="2x" />
+              <FontAwesomeIcon icon={faDocker} size="2x" width="40" />
               <span className="sr-only">PHP</span>
             </li>
             <li>
-              <FontAwesomeIcon icon={faLinux} size="2x" />
+              <FontAwesomeIcon icon={faLinux} size="2x" width="28" />
               <span className="sr-only">Docker</span>
             </li>
             <li>
-              <FontAwesomeIcon icon={faApple} size="2x" />
+              <FontAwesomeIcon icon={faApple} size="2x" width="24" />
               <span className="sr-only">Apple</span>
             </li>
           </ul>
@@ -91,20 +91,20 @@ export default function Home() {
             list is getting too long. Why don't you get in touch with me so we
             can talk more?
           </p>
-        </section>
+        </article>
         <section className="contact">
           <a href="mailto:hello@robins.nu">
-            <FontAwesomeIcon icon={faEnvelope} size="2x" />
+            <FontAwesomeIcon icon={faEnvelope} size="2x" width="32" />
           </a>
           <a
             href="https://www.linkedin.com/in/robin-nilsson-5248a64b"
             target="_blank"
             rel="noreferrer"
           >
-            <FontAwesomeIcon icon={faLinkedin} size="2x" />
+            <FontAwesomeIcon icon={faLinkedin} size="2x" width="28" />
           </a>
           <a href="https://github.com/sn00k" target="_blank" rel="noreferrer">
-            <FontAwesomeIcon icon={faGithub} size="2x" />
+            <FontAwesomeIcon icon={faGithub} size="2x" width="32" />
           </a>
         </section>
       </div>
